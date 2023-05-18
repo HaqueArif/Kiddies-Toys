8abc59c46b394e6b19f66297b1393d59cb605cc0
HaqueArif <haquearif143@gmail.com>
2023-05-18 21:16:54 +0600

    added all toys page'

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,31 +1,47 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import AlltoysCard from "./AlltoysCard";
 
 const AllToys = () => {
 
     const [toys, SetToys] = useState([]);
 
-    useEffect( () => {
+    useEffect(() => {
         fetch('data.json')
-        .then(res=> res.json())
-        .then(data=> {
-            console.log(data);
-            SetToys(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                SetToys(data)
+            })
     }, [])
-    
+
     return (
         <div>
-            <h2 className="text-3xl md: text-5xl text-center font-semibold my-5">All Toys</h2>
+            <h2 className="text-3xl md:text-5xl text-center font-semibold my-5">All Toys</h2>
             <p className="md:w-3/5 mx-auto text-center">Discover a world of toys across various categories, including outdoor play, arts and crafts, puzzles, dolls and action figures, educational toys, construction sets, and cuddly plush companions. Each category is designed to engage, inspire, and bring joy to children of all ages.</p>
             <h2>services{toys.length}</h2>
-            <div>
-                {
-                    toys.map(toy=> <AlltoysCard
-                    key={toy._id}
-                    toy={toy}
+
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       
+                    {
+                    toys.map(toy => <AlltoysCard
+                        key={toy.name}
+                        toy={toy}
                     ></AlltoysCard>)
                 }
+                    </tbody>
+                </table>
             </div>
         </div>
     );
--- src/Pages/AllToys/AlltoysCard.jsx
+++ src/Pages/AllToys/AlltoysCard.jsx
@@ -1,9 +1,18 @@
 
 
-const AlltoysCard = () => {
+const AlltoysCard = ({ toy }) => {
+
+    const { photo_url, name, seller_name, seller_email, price, rating, available_quantity, detail_description, category} = toy;
+
     return (
         <div>
-            
+            <tr>
+                <th>Toy Name: {name}</th>
+                <td>Seller Name: {seller_name}</td>
+                <td>Sub Category: {category}</td>
+                <td>Price: ${price}</td>
+                <td>Available Quantity : ${available_quantity}</td>
+            </tr>
         </div>
     );
 };
